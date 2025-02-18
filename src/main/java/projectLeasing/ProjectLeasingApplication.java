ca22ff15274cf123a7b8113b33dc150f3128edd9
Arda Acar <108298837+AcarardaX1@users.noreply.github.com>
2025-02-18 21:58:18 +0100

    CheckTrial

---
+++ src/main/java/projectLeasing/ProjectLeasingApplication.java
@@ -0,0 +1,13 @@
+package projectLeasing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLeasingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectLeasingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projectLeasing/ProjectLeasingApplicationTests.java
@@ -0,0 +1,13 @@
+package projectLeasing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLeasingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
